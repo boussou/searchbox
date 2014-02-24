a54bd85154d679b888c0aedb3cc577fb8dd55ec9
boussou <boussou@users.noreply.github.com>
2014-02-24 20:36:08 +0100

    JSON response & multiple input fields handling
    ADDED JSON response & multiple input fields handling & processing through jquery tmpl (http://ejohn.org/blog/javascript-micro-templating/ & one of its forks like https://github.com/BorisMoore/jquery-tmpl)

--- searchbox.js
+++ searchbox.js
@@ -1,6 +1,33 @@
 // Author: Ryan Heath
 // http://rpheath.com
 
+/**
+ADDED JSON response handling & multiple input fields handling  & processing through jquery tmpl (http://ejohn.org/blog/javascript-micro-templating/ & one of its forks like https://github.com/BorisMoore/jquery-tmpl)
+
+changes: data & tmpl
+- data : jquery selector for collecting data to submit
+- tmpl : if you use the john resig tmpl, & the server returns back JSON, then it is evaluated for every row
+now you can call it like this:
+
+$('input.search').searchbox({
+  url: your_url,
+  param: 'q',
+  dom_id: '#resultTable',
+  delay: 250,
+  data: 'input.search',
+  tmpl: '<tr> <td>${brand} </td>    <td>${model} </td>  </tr>'
+})
+
+
+if you have inputs like this:
+    <input type="text" id=brand class="form-control search">
+    <input type="text" id=model class="form-control search">
+then you will receive in the GET request values as: brand=value&model=value
+for all the input that are defined by the 'data' parameter
+so you can have multiple fields & still use a live search form
+
+*/
+
 (function($) {
   $.searchbox = {}
   
@@ -10,7 +37,9 @@
       param: 'query',
       dom_id: '#results',
       delay: 100,
-      loading_css: '#loading'
+      loading_css: '#loading',
+      data: null,
+      tmpl: null
     },
     
     loading: function() {
@@ -31,10 +60,28 @@
         base = path[0], params = path[1], query_string = query
       
       if (params) query_string = [params.replace('&amp;', '&'), query].join('&')
-      
+
+      alldata={};
+      if ($.tmpl && $.searchbox.settings.data)
+      $(  $.searchbox.settings.data ).each( function(id,ctrl) { alldata[ ctrl.id ]=ctrl.value;  } );
+     
+      if ($.tmpl && $.searchbox.settings.tmpl)
+        $.ajax({
+          dataType: "json",
+          data: alldata,
+          url: [base, '?', query_string].join(''),
+          success: function(data) {
+               $($.searchbox.settings.dom_id).html( $.tmpl($.searchbox.settings.tmpl, data) );
+          }
+        })
+    else
+    
       $.get([base, '?', query_string].join(''), function(data) {
         $($.searchbox.settings.dom_id).html(data)
       })
+                                                                                  
+                                                                                  
+      
     },
     
     start: function() {
